a519e3cb57661398b2e0d7f768a09dfa0b334ae4
Nadi_Xaq <nporeba7@gmail.com>
2024-10-04 12:12:18 +0200

    Added function to add dots

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-function changeColor() {
+function changeColor() 
+{
     const bigDot = document.getElementById('big-red-dot');
     bigDot.style.backgroundColor = 'blue';
     bigDot.textContent = "BIG RED DOT";
+}
+
+function addDot()
+{
+    newDot = document.createElement('div');
+    newDot.id = 'big-red-dot';
+    document.body.appendChild(newDot);
+    document.getElementById('kw').appendChild(newDot);
 }
