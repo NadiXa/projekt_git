df07804c78c94a88d1ca68340f1495067c4f39ea
Nadi_Xaq <nporeba7@gmail.com>
2024-10-04 12:49:21 +0200

    Added 'random dot' function

--- script.js
+++ script.js
@@ -7,10 +7,21 @@ function changeColor()
 
 function addDot()
 {
-    newDot = document.createElement('div');
-    newDot.id = 'big-red-dot';
-    document.body.appendChild(newDot);
-    document.getElementById('kw').appendChild(newDot);
+    const min = 1;
+    const max = 10;
+    const liczba = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    // newDot = document.createElement('div');
+    // newDot.id = 'big-red-dot';
+    // document.body.appendChild(newDot);
+    // document.getElementById('kw').appendChild(newDot);
+
+    for(let i=0; i<liczba; i++)
+    {
+        newDot = document.createElement('div');
+        newDot.id = 'big-red-dot';
+        document.getElementById('kw').appendChild(newDot);
+    }
 }
 
 function deleteDot()
