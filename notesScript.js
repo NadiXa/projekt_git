9e6cd9437682cc7c7fa2573f0f2bf6028402cf92
Nadi_Xaq <nporeba7@gmail.com>
2024-10-09 12:37:09 +0200

    Added function addNote

--- notesScript.js
+++ notesScript.js
@@ -5,3 +5,29 @@ const notes =
     {id: 3, note: 'ghi'}
 ];
 
+function createNoteTemplate(id, note)
+{
+    return {
+        id: id,
+        title: `Notatka ${id}`,
+        body: note,
+        createdAt: new Date().toISOString()
+    };
+}
+
+function addNote(notes)
+{
+    const cards = [];
+    
+    notes.forEach(element =>
+    {
+        const content = createNoteTemplate(element.id, element.note);
+        cards.push(content);
+    });
+
+    return cards;
+}
+
+const cards = addNote(notes);
+
+console.log(cards);
