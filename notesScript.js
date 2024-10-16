2eb0f3256edb85be90fbd665712814932a5b3cd3
Nadi_Xaq <nporeba7@gmail.com>
2024-10-16 11:15:44 +0200

    Added async functions

--- notesScript.js
+++ notesScript.js
@@ -38,3 +38,43 @@ function addNote(notes)
 }
 
 addNote(notes);
+
+// async functions
+
+async function fetchData() 
+{
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Dane zostały pobrane.");
+        }, 2000); //simulating delay
+    });
+}
+
+async function showData()
+{
+    const dane = await fetchData();
+    console.log(dane);
+}
+
+showData();
+
+//async function with open API
+
+async function fetchPublicData() 
+{
+    try 
+    {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts/1');
+        if(!response.ok)
+        {
+            throw new Error('Błąd HTTP: $response.status');
+        }
+        const data = await response.json();
+        console.log("Pobrane dane: ", data);
+    } catch (error)
+    {
+        console.error("Wystąpił błąd: ", error);
+    }
+}
+
+fetchPublicData();
